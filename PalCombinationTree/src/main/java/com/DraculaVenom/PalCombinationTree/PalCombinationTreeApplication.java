a5afbe03ed1f19b1cce43bf362e5df57b023930e
Juan RSF <juanantunez@rsfinancieros.com>
2025-08-11 11:51:12 -0400

    Initial commit and start of the proyect

---
+++ PalCombinationTree/src/main/java/com/DraculaVenom/PalCombinationTree/PalCombinationTreeApplication.java
@@ -0,0 +1,13 @@
+package com.DraculaVenom.PalCombinationTree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalCombinationTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalCombinationTreeApplication.class, args);
+	}
+
+}
