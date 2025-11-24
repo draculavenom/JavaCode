6331f90aa0c68d44f3326fef79a09910dffbd9dc
Juan RSF <juanantunez@rsfinancieros.com>
2025-11-24 16:19:51 -0500

    Updates to the config.txt file to remove some tabs update in the main file to be able to create the first users

--- schedulingSystemSecured/src/main/java/com/draculavenom/SecurityApplication.java
+++ schedulingSystemSecured/src/main/java/com/draculavenom/SecurityApplication.java
@@ -3,17 +3,17 @@ package com.draculavenom;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.web.cors.CorsConfiguration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//import com.draculavenom.security.auth.AuthenticationService;
-//import com.draculavenom.security.auth.RegisterRequest;
-//import org.springframework.boot.CommandLineRunner;
-//import org.springframework.context.annotation.Bean;
-//import static com.draculavenom.security.user.Role.ADMIN;
-//import static com.draculavenom.security.user.Role.MANAGER;
+import com.draculavenom.security.auth.AuthenticationService;
+import com.draculavenom.security.auth.RegisterRequest;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import static com.draculavenom.security.user.Role.ADMIN;
+import static com.draculavenom.security.user.Role.MANAGER;
 
 @SpringBootApplication
 public class SecurityApplication {
@@ -36,14 +36,14 @@ public class SecurityApplication {
 		};
 	}*/
 
-	/*@Bean
+	@Bean
 	public CommandLineRunner commandLineRunner(//This bean can create by default two users with admin and manager roles.
 			AuthenticationService service
 	) {
 		return args -> {
 			var admin = RegisterRequest.builder()
-					.firstname("Admin")
-					.lastname("Admin")
+					.firstName("Admin")
+					.lastName("Admin")
 					.email("admin@mail.com")
 					.password("password")
 					.role(ADMIN)
@@ -51,8 +51,8 @@ public class SecurityApplication {
 			System.out.println("Admin token: " + service.register(admin).getAccessToken());
 
 			var manager = RegisterRequest.builder()
-					.firstname("Admin")
-					.lastname("Admin")
+					.firstName("Admin")
+					.lastName("Admin")
 					.email("manager@mail.com")
 					.password("password")
 					.role(MANAGER)
@@ -60,5 +60,5 @@ public class SecurityApplication {
 			System.out.println("Manager token: " + service.register(manager).getAccessToken());
 
 		};
-	}*/
+	}
 }
