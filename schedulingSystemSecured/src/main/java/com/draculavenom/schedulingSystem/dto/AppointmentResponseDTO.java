eccba35580441fdba5616dcb61cf7b31f8c09645
Leslie <leslieksalinasran@gmail.com>
2026-01-25 20:24:25 -0600

    List of appointments with company name

---
+++ schedulingSystemSecured/src/main/java/com/draculavenom/schedulingSystem/dto/AppointmentResponseDTO.java
@@ -0,0 +1,29 @@
+package com.draculavenom.schedulingSystem.dto;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import com.draculavenom.schedulingSystem.model.AppointmentStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AppointmentResponseDTO {
+    private Integer appointmentId;
+    private LocalDate date;
+    private LocalTime time;
+    private AppointmentStatus status;
+
+    private Integer userId;
+
+    private Integer managerId;
+    private String companyName;
+}
