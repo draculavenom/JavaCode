eb670b3ac63fd2737117ee4a7dba5cf8abd0c0a8
Leslie <leslieksalinasran@gmail.com>
2026-03-19 12:28:49 -0600

    Additional information from comments

--- schedulingSystemSecured/src/main/java/com/draculavenom/notification/service/AppointmentNotificationService.java
+++ schedulingSystemSecured/src/main/java/com/draculavenom/notification/service/AppointmentNotificationService.java
@@ -49,6 +49,14 @@ public class AppointmentNotificationService {
             default -> "hass been updated";
         };
 
+        String commentsSection = switch(status){
+            case CONFIRMED -> "\nAdditional info:\n" + appointment.getComment() + "\n";
+            case CANCELLED -> "\nReason for cancellation:\n" + appointment.getComment() + "\n";
+            case COMPLETED -> "\nManager notes:\n" + appointment.getComment() + "\n";
+            default -> "\nComments:\n" + appointment.getComment() + "\n";
+        };
+        
+
         String body = """
             Hello %s,
 
@@ -58,13 +66,14 @@ public class AppointmentNotificationService {
             Time: %s 
 
             %s
+            %s
             
             If you have any questions, please contact support.
             """.formatted(
                 user.getName(), 
                 appointment.getDate(),
                 appointment.getTime(),
-                statusMessage
+                statusMessage, commentsSection
             );
 
             emailService.sendSimpleMessage(user.getEmail(), subject, body);
